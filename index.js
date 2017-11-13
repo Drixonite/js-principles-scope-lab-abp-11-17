c7e4515170dcc6a779aa3d61d8b8cfb0897ab5b5
David Kim <mail.ddk@gmail.com>
2017-11-13 04:10:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+function customerName() {
+  var customerName = 'bob';
+  return customerName
+}
+var customerName = 'bob';
+
+function upperCaseCustomerName(customerName){
+  customerName.toUpperCase(bob);
+  return customerName;
+}
+
+function setBestCustomer() {
+  var bestCustomer == 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer === bob;
+}
+
+const leastFavoriteCustomer = ddk;
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer === houngh;
+}
